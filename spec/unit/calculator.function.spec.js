3e44778df45bb0c424fd7d54d7598ae76a6a0995
AnkushLambdatest <107558074+AnkushLambdatest@users.noreply.github.com>
2022-08-01 13:21:37 +0530

    Update calculator.function.spec.js

--- spec/unit/calculator.function.spec.js
+++ spec/unit/calculator.function.spec.js
@@ -22,7 +22,7 @@ describe("Calculator Functions", function() {
 
     it("should add two positive numbers", function() {
       let value = calc.add(3, 2);
-      expect(value).toBe(5);
+      expect(value).toBe(6);
     });
   
     it("should add a positive and a negative number", function() {
